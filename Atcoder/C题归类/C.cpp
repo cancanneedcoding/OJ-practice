5c2eb3a4dce7b4b47c99a15324422b88f65b5c00
zhigeng <84454978+13542718259@users.noreply.github.com>
2022-11-07 23:14:26 +0800

    Add files via upload

--- Atcoder/C题归类/C.cpp
+++ Atcoder/C题归类/C.cpp
@@ -2,32 +2,49 @@
 using namespace std;
 typedef long long LL;
 typedef unsigned long long uLL;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
+#define se second;
+#define fi first;
 #define pb(s) push_back(s);
-#define ms(s) memset(s, 0, sizeof(s))
+#define ms(s,x) memset(s, x, sizeof(s))
+#define all(s) s.begin(),s.end()
 const int inf = 0x3f3f3f3f;
 const int mod = 1000000007;
-const int N=100010;
+const int N = 200010;
 
-LL n;
-set<int> ans;
-int main() 
+int n;
+LL s[N], t[N], res[N];
+void solve()
 {
-	cin>>n;
-	int end=0;
-	for(int i=0;i<64;++i){
-		if((n>>i)&1){
-			ans.insert(i);
-			end=i;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> s[i];
+	}
+	int x = -1;
+	LL ans = 1e10;
+	for (int i = 0; i < n; ++i) {
+		cin >> t[i];
+		if (t[i] < ans) {
+			x = i;
+			ans = t[i];
 		}
 	}
-	string s;
-	for(int i=end;i>=0;i--){
-		if(ans.count(i)){
-			   s+="BA"; 
-		}
-		else s+="B";
+	res[x] = t[x];
+	for (int i = 0; i < n - 1; ++i) {
+		int ne = (x + 1) % n;
+		res[ne] = min(t[ne], res[x] + s[x]);
+		x = ne;
 	}
-	cout<<s<<'\n';
-    return 0;
+	for (int i = 0; i < n; ++i) cout << res[i] <<'\n';
+}
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
 }
