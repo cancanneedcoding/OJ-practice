88362032686f66d3187815c79ae54783351e4a9f
zhigeng <84454978+13542718259@users.noreply.github.com>
2022-11-17 23:26:29 +0800

    Create 吃掉 N 个橘子的最少天数.cpp

---
+++ LeetCode/吃掉 N 个橘子的最少天数.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    unordered_map<int,int> memo;
+    int minDays(int n) {
+       if (n <= 1) {
+            return n;
+        }
+        if (memo.count(n)) {
+            return memo[n];
+        }
+        return memo[n] = min(n % 2 + 1 + minDays(n / 2), n % 3 + 1 + minDays(n / 3));
+    }
+};
